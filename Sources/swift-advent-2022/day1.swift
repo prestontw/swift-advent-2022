6ce3ed7acda687be57a671b3a425f36434a71983
Preston TW <preston_tunnell_wilson@brown.edu>
2023-02-20 19:34:43 -0500

    Add test for day1

--- Sources/swift-advent-2022/day1.swift
+++ Sources/swift-advent-2022/day1.swift
@@ -1,4 +1,3 @@
 public func parseLine(_ s: String) -> [String] {
-  let s = s.split(separator: " ").map({String($0)})
-  return s
+  s.split(separator: " ").map({String($0)})
 }
--- Tests/swift-advent-2022Tests/swift_advent_2022Tests.swift
+++ Tests/swift-advent-2022Tests/swift_advent_2022Tests.swift
@@ -1,12 +1,20 @@
 import XCTest
 @testable import swift_advent_2022
 
+let x = ["test", "message"]
+
 final class swift_advent_2022Tests: XCTestCase {
     func testExample() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
         XCTAssertEqual(swift_advent_2022().text, "Hello, World!")
-        XCTAssertEqual(parseLine("test message"), ["test", "message"])
+        XCTAssertEqual(parseLine("test message"), x)
+    }
+}
+
+final class day1Tests: XCTestCase {
+    func testCases() throws {
+        XCTAssertEqual(parseLine("test message"), x)
     }
 }
